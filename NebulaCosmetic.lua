63dd6499b5c27768f950970691099abf2eee18c5
nyuwaea <62360678+nyuwaea@users.noreply.github.com>
2023-04-18 14:19:28 -0400

    Update NebulaCosmetic.lua

--- NebulaCosmetic.lua
+++ NebulaCosmetic.lua
@@ -2,18 +2,26 @@
 			Handles all the effects on the Nebula ladder
 ]]
 
-local ladder = script.Parent.Parent
-local particles = ladder.Cosmetics.Particles
+------------► ► ►	SERVICES	◄ ◄ ◄------------
 
-particles.Parent = ladder
+local RunService = game:GetService("RunService")
 
-game:GetService("RunService").RenderStepped:Connect(function()
+------------► ► ►	VARIABLES	◄ ◄ ◄------------
+
+local Ladder = script.Parent.Parent
+local Particles = Ladder.Cosmetics.Particles
+
+Particles.Parent = Ladder
+
+------------► ► ►	FUNCTIONS	◄ ◄ ◄------------
+
+RunService.RenderStepped:Connect(function(): ()
 	local alpha = (math.sin(tick() * 2) + 1) / 2
-	local mainCF = ladder.PrimaryPart.CFrame
+	local mainCF = Ladder.PrimaryPart.CFrame
 	
-	ladder.Cosmetics.Highlight.OutlineColor = Color3.fromHex("ff64b1"):Lerp(Color3.fromHex("a564ff"), alpha)
+	Ladder.Cosmetics.Highlight.OutlineColor = Color3.fromHex("ff64b1"):Lerp(Color3.fromHex("a564ff"), alpha)
 
-	for i, particle in ipairs(particles:GetChildren()) do
+	for i, particle in ipairs(Particles:GetChildren()) do
 		local distance = i == 1 and 5 or -5
 		local targetCF = CFrame.new(mainCF.Position + mainCF.RightVector * distance * math.sin(tick()) + mainCF.LookVector * distance * math.cos(tick()) + mainCF.UpVector * distance * .8 * math.sin(tick() / 4))
 
