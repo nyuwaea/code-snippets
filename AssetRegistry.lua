6e71c15ec4886b6fa55eb6813776e6b33664f40b
nyuwaea <62360678+nyuwaea@users.noreply.github.com>
2023-04-18 14:00:11 -0400

    Update AssetRegistry.lua

--- AssetRegistry.lua
+++ AssetRegistry.lua
@@ -2,16 +2,24 @@
 		This module handles all the assets to be used in a procedural generation game
 ]]
 
+------------► ► ►	SERVICES	◄ ◄ ◄------------
+
+local ServerStorage = game:GetService("ServerStorage")
+
+------------► ► ►	VARIABLES	◄ ◄ ◄------------
+
+local AssetObjects = ServerStorage.Assets
+
+------------► ► ►	 MODULE		◄ ◄ ◄------------
+
 local Registry = {}
 Registry.__index = Registry
 
-local AssetObjects = game:GetService("ServerStorage").Assets
-
-function Registry.Setup()
+function Registry.Init(): {}
 	local self = setmetatable({}, Registry)
 
 	self._assets = {}				-- The main table containing all assets
-	self._difficulties = {}			-- The table containing indexable difficulties
+	self._difficulties = {}				-- The table containing indexable difficulties
 	self._tags = {}					-- The table containing all tags, similarly to CollectionService
 	
 	self:Add(1, "Start", 0, 0, 0, false, {})
@@ -28,7 +36,7 @@ function Registry.Setup()
 	return self
 end
 
-function Registry:Add(ID : number, name : string, difficulty : number, probability : number, heightIncrement : number, canGenerate : boolean, tags : {})
+function Registry:Add(ID: number, name: string, difficulty: number, probability: number, heightIncrement: number, canGenerate: boolean, tags: {}): ()
 	self._assets[ID] = {name = name; probability = probability; heightIncrement = heightIncrement; canGenerate = canGenerate; object = AssetObjects[name]; starts = AssetObjects[name].Technical.Starts:GetChildren(); exits = AssetObjects[name].Technical.Exits:GetChildren()}
 	self._difficulties[difficulty] = self._difficulties[difficulty] or {}
 	
@@ -42,16 +50,16 @@ function Registry:Add(ID : number, name : string, difficulty : number, probabili
 	end
 end
 
-function Registry:Get(ID : number?)
+function Registry:Get(ID: number?): {}
 	return ID and self._assets[ID] or self._assets
 end
 
-function Registry:GetDifficulty(difficulty : number)
+function Registry:GetDifficulty(difficulty: number): {}
 	return self._difficulties[difficulty]
 end
 
-function Registry:GetTag(tag : string)
+function Registry:GetTag(tag: string): {}
 	return self._tags[tag]
 end
 
-return Registry
+return Registry.Init()
